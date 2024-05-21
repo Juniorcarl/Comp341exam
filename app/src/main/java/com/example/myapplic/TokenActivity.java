117e77e6162affa02faef0c18adc93af02a418e8
INEELENG BAGALATIA <BI22000511@BIUST.AC.BW>
2024-05-21 15:01:48 +0200

    initial commit

---
+++ app/src/main/java/com/example/myapplic/TokenActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplic;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TokenActivity extends AppCompatActivity {
+
+    TextView value;
+
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_token);
+
+        value = findViewById(R.id.toiletc);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+  public  void increment(View v){
+        count++;
+        value.setText(""+count);
+
+  }
+
+  public  void decrement(View v){
+        if(count<=0) count=0;
+        else count--;
+        value.setText(""+count);
+
+  }
+}
